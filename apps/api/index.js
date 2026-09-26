6ce8948691dd5076c1f6c88eb52599711255fff9
Josef Tinagan <tinaganjosefperry@gmail.com>
2026-09-26 13:59:00 +0800

    feat: add and setup prisma orm configuration and connect it with the backend @repo/api with basic setup

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,11 +1,22 @@
 import express from 'express';
+import { prisma } from "@repo/database"
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.status(200).json({ name: "frodo"})
+})
+
 app.get('/api', (req, res) => {
   res.json({ message: "Hello from the Node.js backend!" });
 });
+
+app.get('/user', async (req, res) => {
+  const user = await prisma.user.findFirst();
+  console.log(user);
+  res.status(200).json({ userId: user.id, username: user.username, password: user.password });
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
